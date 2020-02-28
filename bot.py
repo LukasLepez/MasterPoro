19c378708fdf56165a9b18e5e71073fd1d888b8b
LEPEZ <lukas.lepez@capgemini.com>
2020-02-28 10:30:52 +0100

    Mise en place des normes pylint

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+""" Regroup all MasterPoro commands """
+
 import os
 import sys
-import discord
 
 from dotenv import load_dotenv
 from discord.ext import commands as botCommands
-from functions import guildsLanguage
+from functions import guilds_language
 
 sys.path.append('lang')
 load_dotenv()
@@ -19,19 +20,21 @@ BOT = botCommands.Bot(command_prefix='$')
 # Function to say hello
 @BOT.command()
 async def hello(ctx):
+    """ Command to say hello """
     id_guilds = str(BOT.guilds[0].id)
-    language_module = guildsLanguage.check_guilds_language(id_guilds)
+    language_module = guilds_language.check_guilds_language(id_guilds)
     await ctx.send(language_module.hello_message)
 
 
 # Function that changes the language of server
 @BOT.command()
 async def lang(ctx, arg):
+    """ Command to change language of discord server """
     id_guilds = str(BOT.guilds[0].id)
 
-    guildsLanguage.check_guilds_language(id_guilds)
+    guilds_language.check_guilds_language(id_guilds)
 
-    result = guildsLanguage.change_guilds_language(arg, id_guilds)
+    result = guilds_language.change_guilds_language(arg, id_guilds)
 
     await ctx.send(result)
 
---
+++ functions/guilds_language.py
@@ -0,0 +1,59 @@
+""" Allows you to manage the language of a server discord (guilds) """
+
+import json
+import importlib
+
+def check_guilds_language(id_guilds):
+    """ Function that checks the language of servers (guilds) """
+    with open('./lang/server.json', 'r') as json_file_read:
+        lang_json_r = json.load(json_file_read)
+
+    lang_result = list(filter(lambda x: x == str(id_guilds), lang_json_r))
+    if lang_result == []:
+        data = lang_json_r
+        new_data = {
+            id_guilds: {
+                "language": "en"
+            },
+        }
+        data.update(new_data)
+
+        with open("./lang/server.json", "w") as json_file_write:
+            json.dump(data, json_file_write)
+
+        json_file_write.close()
+        lang_guilds = 'en'
+
+    else:
+        lang_guilds = lang_json_r[id_guilds]['language']
+
+    language_module = __import__(lang_guilds)
+
+    json_file_read.close()
+
+    return language_module
+
+
+
+def change_guilds_language(arg, id_guilds):
+    """ Function that change the language of servers (guilds) """
+    if arg in ("fr", "en"):
+        with open('lang/server.json', 'r') as json_file_read:
+            lang_json_r = json.load(json_file_read)
+
+        lang_json_r[id_guilds]['language'] = arg
+
+        with open('lang/server.json', 'w') as json_file_write:
+            json.dump(lang_json_r, json_file_write)
+
+        json_file_read.close()
+        json_file_write.close()
+
+        language_module = check_guilds_language(id_guilds)
+
+        importlib.reload(language_module)
+
+        return language_module.success_language_message
+
+    language_module = check_guilds_language(id_guilds)
+    return language_module.error_language_message
